21ec21af4a2c9fe9596634cb76d4ceb209cbebde
Sneha Gaurav Deshpande <sneha.10821781@ltimindtree.com>
2024-10-18 13:02:17 +0530

    DTO package name correction

--- atm/src/main/java/com/hackathon/orangepod/atm/controller/AccountController.java
+++ atm/src/main/java/com/hackathon/orangepod/atm/controller/AccountController.java
@@ -15,9 +15,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/atm/accounts")
-@CrossOrigin(origins = "http://localhost:4200")
 public class AccountController {
 
     @Autowired
--- atm/src/main/java/com/hackathon/orangepod/atm/controller/UserController.java
+++ atm/src/main/java/com/hackathon/orangepod/atm/controller/UserController.java
@@ -10,9 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/atm/user")
-@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
